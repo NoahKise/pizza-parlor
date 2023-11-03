10afdbbd723cf9b7cb2c0dc1c021245255146ae0
noahkise <noah@kisefamily.com>
2023-11-03 10:54:32 -0700

    add form handler to gather user inputs

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+// Business Logic
+
 function Pizza(size, sauce, cheese, veggieToppings, meatToppings) {
   this.size = size;
   this.sauce = sauce;
@@ -6,7 +8,7 @@ function Pizza(size, sauce, cheese, veggieToppings, meatToppings) {
   this.meatToppings = meatToppings;
 }
 
-Pizza.prototype.costCalculate = function() {
+Pizza.prototype.costCalculate = function () {
   let pizzaCost = 0;
   if (this.size === "large") {
     pizzaCost += 20;
@@ -18,3 +20,28 @@ Pizza.prototype.costCalculate = function() {
   return pizzaCost;
 };
 
+// UI Logic
+
+function formHandler(event) {
+  event.preventDefault();
+  const inputSize = document.querySelector("select#size").value;
+  const inputSauce = document.querySelector("select#sauce").value;
+  const inputCheese = document.querySelector("select#cheese").value;
+  let inputVeggieToppings = [];
+  let possibleVeggieToppings = document.getElementsByName("vegTop");
+  console.log(possibleVeggieToppings.length);
+  for (let i = 0; i < possibleVeggieToppings.length; i++) {
+    if (possibleVeggieToppings[i].checked) {
+      inputVeggieToppings.push(possibleVeggieToppings[i].value);
+    }
+  }
+  console.log(inputVeggieToppings);
+  return inputVeggieToppings;
+
+}
+
+
+
+window.addEventListener("load", function () {
+  document.querySelector("form#pizzaBuilder").addEventListener("submit", formHandler);
+});
